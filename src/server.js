89147e5e5958b5b86ca62b3e9b1f4ede602906eb
Dmytro1573 <124176235+Dmytro1573@users.noreply.github.com>
2024-09-21 23:34:14 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,11 +1,44 @@
+import express from "express";
+import cors from "cors";
+import eventsRouter from "../src/routes/events.js";
+import { errorHandler } from "./middlewares/errorHandler.js";
+import { notFoundHandler } from "./middlewares/notFoundHandler.js";
 import "dotenv/config";
 
-import app from "../src/app.js";
+const whitelist = [
+  "https://water-tracker-101-team-5.vercel.app",
+  "http://127.0.0.1:5173",
+];
 
-export const setupServer = () => {
-  const PORT = Number(process.env.PORT) || 3000;
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
+
+const openCorsOptions = {
+  origin: "*",
+  optionsSuccessStatus: 200,
+};
+
+const PORT = Number(process.env.PORT || "3000");
+export function setupServer() {
+  const app = express();
+
+  app.use(cors(corsOptions));
+
+  app.use(eventsRouter);
+
+  app.use(notFoundHandler);
+  app.use(errorHandler);
 
   app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
   });
-};
+}
